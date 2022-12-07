632c31cd378d31504798f510caeba2ea36395a8d
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2022-12-07 15:54:46 +0700

    add method push

--- Exe4_012/Program.cs
+++ Exe4_012/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,17 @@ namespace Exe4_012
         {
             top = -1;
         }
-
+         public void push()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Push");
+                return;
+            }
+            else
+            {
+                top++;
+            }
+        }
     }
 }
